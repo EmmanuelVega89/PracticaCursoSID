9c29d33119aeb753edb0c882287781732d27ad16
EmmanuelVega89 <88913234+EmmanuelVega89@users.noreply.github.com>
2025-11-25 15:58:45 -0600

    feat: create a project base

---
+++ ApiClientTest/Program.cs
@@ -0,0 +1,9 @@
+﻿using ApiClientLibrary.Services;
+
+F1_ConfiguracionInicial apirest = new F1_ConfiguracionInicial();
+var response = await apirest.RegistrarEstadoSID(new ApiClientLibrary.Models.EstadoSIDDTO
+    {
+        Estado = "EN_ESPERA"
+    });
+
+Console.WriteLine(response);
